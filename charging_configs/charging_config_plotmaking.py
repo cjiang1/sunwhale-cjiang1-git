eb766f6300cd4b43ee08d13bb6b1f0287fd9a9e0
Charlie Jiang <charlieyj12@gmail.com>
2014-10-11 12:41:44 -0700

    Updated charging config code

--- charging_configs/charging_config_plotmaking.py
+++ charging_configs/charging_config_plotmaking.py
@@ -3,7 +3,7 @@
 
 ## SSCP | Sunwhale | Charging Configuration Analysis
 
-# In[99]:
+# In[41]:
 
 get_ipython().magic(u'pylab inline')
 import numpy as np
@@ -11,58 +11,63 @@ import math
 import matplotlib.pyplot as plt
 
 inToM = 0.0254 # Inches -> meters
+NPOINTS = 1000 # Number of points to compute in each calculation
+
+HB = 2.2 # Bounding box height
+WB = 1.8 # Bounding box width
+L = 12*inToM # Est. concentrator element length when fully deployed (lens + cell assembly + focal length + enclosure)
+W = 7*inToM # Est. conc. element width when deployed (lens width + enclosure; assume element is square)
 
 def compute_d(wc, hc, theta_deg):
     """
     Compute d (the length of the space available for concentrators) for the given ndarray of angles
     """
-    hb = 2.2 # Bounding box height
-    wb = 1.8 # Bounding box width
-    l = 12*inToM # Est. concentrator element length when fully deployed (lens + cell assembly + focal length + enclosure)
-    
     theta_rad = theta_deg*(math.pi/180)
-    return wb*np.sin(theta_rad) + hb*np.cos(theta_rad) - ((hc+l)/2)*np.sin(2*theta_rad) - wc
+    return WB*np.sin(theta_rad) + HB*np.cos(theta_rad) - ((hc+L)/2)*np.sin(2*theta_rad) - wc
 
 def compute_nrows(d):
     """
     Compute the number of rows of concentrators that can fit for the given ndarray of d
     """
-    w = 7*inToM # Est. conc. element width when deployed (lens width + enclosure; assume element is square)
-    return np.floor(d/w)
+    return np.floor(d/W)
 
 # Get a set of angles over which to compute (Max determined we need to consider 0-30 degrees)
-theta = np.linspace(0,30,num=1001)
+theta = np.linspace(0,30,num=NPOINTS+1)
 theta = np.delete(theta,0)
 
 
 ## Fitting concentrators for monocoque
 
-# In[133]:
+# In[42]:
 
 # Plot d, the space at the top of box available for concentrators (in m)
-d = compute_d(1.8,0.55,theta)
-plt.plot(theta,d)
+d_monocoque = np.zeros((1,NPOINTS))
+d_monocoque[0] = compute_d(1.8,0.55,theta)
+plt.plot(theta,d_monocoque[0])
 plt.xlabel(r"$\theta$",fontsize=16)
 plt.ylabel("d",fontsize=16)
 
 
-# In[152]:
+#### Rows of concentrators vs. angle for monocoque
+
+# In[43]:
 
 # Plot number of rows of concentrators that can fit
-nrows = compute_nrows(d)
-plt.plot(theta,nrows)
+nrows_monocoque = np.zeros((1,NPOINTS))
+nrows_monocoque[0] = compute_nrows(d_monocoque)
+plt.plot(theta,nrows_monocoque[0])
 plt.xlabel(r"$\theta$",fontsize=16)
 plt.ylabel("# rows",fontsize=16)
 
 # Smallest angle that allows three rows to fit
-threeRowsIs = np.where(nrows == 3)
+threeRowsIs = np.where(nrows_monocoque[0] == 3)
 minThetaThreeRows = theta[threeRowsIs[0][0]]
-print "Smallest angle for three rows = ",minThetaThreeRows
+print "Smallest angle for three rows =",minThetaThreeRows,"degrees"
 
 
 ### Fitting concentrators for topshell
 
-# In[147]:
+# In[44]:
 
 # Compute
 topshell_widths = linspace(1.5,1.8,4) # Range of possible topshell widths over which to compute
@@ -75,7 +80,18 @@ for i in range(0, topshell_widths.size):
 nrows_data = compute_nrows(d_data)
 
 
-# In[151]:
+# In[45]:
+
+# Append monocoque data for comparison in plots
+# Only run this once with the previous cell
+topshell_widths = numpy.append(topshell_widths, "Monocoque")
+nrows_data = numpy.append(nrows_data,nrows_monocoque,axis=0)
+d_data = numpy.append(d_data,d_monocoque,axis=0)
+
+
+#### Rows of concentrators for various topshell widths
+
+# In[46]:
 
 # Plot nrows vs. topshell_width vs. theta
 fig,ax = plt.subplots()
@@ -90,7 +106,15 @@ plt.title("Rows of concentrators for various topshell widths, 0-30 degrees",font
 print "Topshell 'height' = ",topshell_height
 
 
+# In[47]:
+
+# What is the maximum topshell width to be able to fit three rows at 0 degrees?
+print "Max topshell width for 3 rows at vertical (m) = ", HB-3*W
+
+
+###  Performance vs. # Concentrators
+
 # In[ ]:
 
-
+# Number 
 
